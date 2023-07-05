ce3e135c93f73fa01c6678d452c6c90d3261784d
Justin <jkimble.dev@gmail.com>
2023-07-05 19:31:44 -0400

    edit aqi controller, view

--- src/main/java/jkimble/weatherapp/AQIController.java
+++ src/main/java/jkimble/weatherapp/AQIController.java
@@ -2,6 +2,7 @@ package jkimble.weatherapp;
 
 import jakarta.validation.Valid;
 
+import org.springframework.boot.autoconfigure.batch.BatchProperties.Job;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.ui.Model;
@@ -31,9 +32,54 @@ public class AQIController implements WebMvcConfigurer {
     public String processCityForm(@Valid CurrentAQI currentAQI, BindingResult bindingResult, Model aqiModel) throws IOException {
 
         if (bindingResult.hasErrors()) {
-            return "form";
+            return "CurrentAQIForm";
         }
+
         aqiModel.addAttribute("city", currentAQI.getCity());
-        return "CityResults";
+        String cityQuery = currentAQI.getCity();
+        JsonNode json;
+        JsonNode aqiData;
+
+        try {
+            json = new ObjectMapper().readTree(new URL("http://api.openweathermap.org/geo/1.0/direct?q=" + cityQuery + "&limit=2&appid=8fe44c03b38ef50e722f5d2c9c2bd80d&units=imperial"));
+        } catch (IOException e) {
+            return "CurrentAQIForm";
+        }
+
+        JsonNode mostRel = json.get(0);
+        JsonNode lat = mostRel.get("lat");
+        JsonNode lon = mostRel.get("lon");
+        JsonNode name = mostRel.get("name");
+        aqiModel.addAttribute("name", name.toString());
+
+        try {
+            aqiData = new ObjectMapper().readTree(new URL("http://api.openweathermap.org/data/2.5/air_pollution?lat=" + lat.toString() + "&lon=" + lon.toString() + "&appid=8fe44c03b38ef50e722f5d2c9c2bd80d&units=imperial"));
+        } catch (IOException e) {
+            return "CurrentAQIForm";
+        }
+
+        JsonNode mainList = aqiData.get("list");
+        
+        Iterator<Map.Entry<String, JsonNode>> fields = mainList.fields();
+        System.out.println(mainList.toString());
+        
+        
+        //System.out.println(aqiData.toString());
+        /*Iterator<Map.Entry<String, JsonNode>> fields = aqiData.fields();
+        while (fields.hasNext()) {
+            Map.Entry<String, JsonNode> field = fields.next();
+            System.out.println(field.getKey() + "=" + field.getValue());
+            JsonNode components = field.getValue();
+            Iterator<Map.Entry<String, JsonNode>> innerComps = components.fields();
+            while (innerComps.hasNext()) {
+                Map.Entry<String, JsonNode> innerComp = innerComps.next();
+                System.out.println(innerComp.getKey() + ":" + innerComp.getValue().asText());
+            }
+        }*/
+
+
+        aqiModel.addAttribute("pageTitle", "Air Quality Index for " + aqiModel.getAttribute("name"));
+        aqiModel.addAttribute("activePage", "active");
+        return "AQIResults";
     }
 }
